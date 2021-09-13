c27460cccb1dadf7828a3245a877f548345c2047
cary <CaryWill.K@gmail.com>
2021-09-13 14:31:22 +0800

    fix: 抽离 header

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 如何新增一片文章
 
-Ulysses 导出 html 后，替换 html 的 css path 为 css dir 下面的 style.css 文件
+1. Ulysses 导出 html 后，替换 html 的 css path 为 css dir 下面的 style.css 文件0
+2. <script src='/js/index.js'></script> 在文章末尾加上
--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
 // TODO: generte tree from folder structure
+(function () {
+  const header = `
+  <div class="header-bar">
+    <div class="site-id">
+      <h1><a href="/">Cary</a></h1>
+    </div>
+
+    <ol class="links">
+      <li>
+        <a href="/index.html">Home</a>
+      </li>
+      <li>
+        <span class="active">Articles</span>
+      </li>
+      <li>
+        <a href="/about/">About</a>
+      </li>
+    </ol>
+  </div>
+  `
+  const div = document.createElement("header");
+  div.innerHTML = header;
+  document.body.prepend(div, document.body.firstChild);
+})();
