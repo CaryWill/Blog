5c1ea8b2ea13189288938530b8805e34ba1c5e7f
cary <CaryWill.K@gmail.com>
2021-10-01 15:34:52 +0800

    fix: add desc:wq

--- js/outline.js
+++ js/outline.js
@@ -1,25 +1,25 @@
 var outline = [
   {
     title: "手写 Promise 及原理解析",
-    desc: "",
+    desc: "通过手写一个 promise 来帮你理解 promise 的原理",
     date: "Sept 28, 2021",
     href: "/src/2021/learn-promise-by-implement/index.html",
   },
   {
     title: "The git internals",
-    desc: "",
+    desc: "对比寓言故事里提到的 git 底层原理和实际的 git 底层原理的差异",
     date: "Sept 20, 2021",
     href: "/src/2021/the-git-internals/index.html",
   },
   {
     title: "The git parable",
-    desc: "",
+    desc: "用一则寓言故事来帮助你了解 git 背后的原理以及命令的使用。",
     date: "Sept 15, 2021",
     href: "/src/2021/the-git-parable/the-git-parable.html",
   },
   {
     title: "Regex 101",
-    desc: "",
+    desc: "Regular expression 101",
     date: "Sept 01, 2021",
     href: "/src/2021/regex101.html",
   },
