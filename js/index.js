3e9efc7f1ef50ccf7ccd9913f8a1bbcef82ff7c6
cary <CaryWill.K@gmail.com>
2021-10-07 11:22:16 +0800

    fix: commen

--- js/index.js
+++ js/index.js
@@ -28,24 +28,13 @@
   commentContainer.innerHTML = container;
   document.body.append(commentContainer);
 
-  const commentInstallCode = `
-  <script>
-    /**
-    *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
-    *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables    */
-    /*
-    var disqus_config = function () {
-    this.page.url = window.location.href;  // Replace PAGE_URL with your page's canonical URL variable
+  window.disqus_config = function () {
+    this.page.url = window.location.href; // Replace PAGE_URL with your page's canonical URL variable
     this.page.identifier = window.location.href; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
-    };
-    */
-    (function() { // DON'T EDIT BELOW THIS LINE
-    var d = document, s = d.createElement('script');
-    s.src = 'https://https-carytalk-netlify-app.disqus.com/embed.js';
-    s.setAttribute('data-timestamp', +new Date());
-    (d.head || d.body).appendChild(s);
-    })();
-</script>
-<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>`;
-  document.body.append(commentInstallCode);
+  };
+  var d = document,
+    s = d.createElement("script");
+  s.src = "https://https-carytalk-netlify-app.disqus.com/embed.js";
+  s.setAttribute("data-timestamp", +new Date());
+  (d.head || d.body).appendChild(s);
 })();
