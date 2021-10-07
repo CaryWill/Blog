e4d98bbf26ec9b0b0cc866dbd0dc2bb9a75e63ba
cary <CaryWill.K@gmail.com>
2021-10-07 11:18:52 +0800

    fix: comment

--- js/index.js
+++ js/index.js
@@ -28,15 +28,15 @@
   commentContainer.innerHTML = container;
   document.body.append(commentContainer);
 
-  document.getElementById("disqus_thread").innerHTML = `
+  const commentInstallCode = `
   <script>
     /**
     *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
     *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables    */
     /*
     var disqus_config = function () {
-    this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
-    this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+    this.page.url = window.location.href;  // Replace PAGE_URL with your page's canonical URL variable
+    this.page.identifier = window.location.href; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
     };
     */
     (function() { // DON'T EDIT BELOW THIS LINE
@@ -47,4 +47,5 @@
     })();
 </script>
 <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>`;
+  document.body.append(commentInstallCode);
 })();
