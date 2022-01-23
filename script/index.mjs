18e4ba31caec825f50e62716b096fc6e2258b58d
cary <CaryWill.K@gmail.com>
2022-01-23 21:14:14 +0800

    fix: remove js

--- script/index.mjs
+++ script/index.mjs
@@ -80,30 +80,30 @@ walk(path.join("./src"), function (err, results) {
         // 转换 tiff 到 png 以免浏览器不能显示
         data = data.replaceAll(".tiff", ".png");
         // 添加一个 script 来保证只有一个
-        [js, js2, assetsjs].forEach((item) => {
-          data = data + item;
-        });
+        // [js, js2, assetsjs].forEach((item) => {
+        //   data = data + item;
+        // });
         fs.writeFileSync(p, data, "utf-8");
       } else if (p.includes(".tiff")) {
-        try {
-          var options = {
-            logLevel: 1,
-          };
-          var converter = new ConvertTiff(options);
-          converter.convertOne(p, path.dirname(p));
-          // 因为转换会创建文件夹 所以需要将里面的文件移出来
-          // https://stackoverflow.com/a/41562625
-          var oldPath = p.split(".tiff")[0] + "/" + "0.png";
-          var newPath = p.split(".tiff")[0] + ".png";
-          if (fs.existsSync(p.split(".tiff")[0])) {
-            fs.renameSync(oldPath, newPath, function (err) {
-              if (err) throw err;
-              console.log("Successfully renamed - AKA moved!");
-            });
-          }
-        } catch (error) {
-          console.log(error);
-        }
+        // try {
+        //   var options = {
+        //     logLevel: 1,
+        //   };
+        //   var converter = new ConvertTiff(options);
+        //   converter.convertOne(p, path.dirname(p));
+        //   // 因为转换会创建文件夹 所以需要将里面的文件移出来
+        //   // https://stackoverflow.com/a/41562625
+        //   var oldPath = p.split(".tiff")[0] + "/" + "0.png";
+        //   var newPath = p.split(".tiff")[0] + ".png";
+        //   if (fs.existsSync(p.split(".tiff")[0])) {
+        //     fs.renameSync(oldPath, newPath, function (err) {
+        //       if (err) throw err;
+        //       console.log("Successfully renamed - AKA moved!");
+        //     });
+        //   }
+        // } catch (error) {
+        //   console.log(error);
+        // }
       } else if (p.includes(".css")) {
         // 清空
         data = "";
